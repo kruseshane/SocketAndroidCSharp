80ad0968f116b0e471003dddf2bd20a2495741d8
kruseshane <32747753+kruseshane@users.noreply.github.com>
2019-03-27 20:17:46 -0400

    Add comments to ConnectionClass.java

--- CSharpSocketApp_Android/app/src/main/java/com/example/csharpsocketapp/ConnectionClass.java
+++ CSharpSocketApp_Android/app/src/main/java/com/example/csharpsocketapp/ConnectionClass.java
@@ -15,7 +15,11 @@ public class ConnectionClass extends AsyncTask<Void, Void, Void> {
         System.out.println("Background process started");
 
         try {
+            // Socket for the android app, used to connect to the socket on the specified device using the specified port
+            // 10.0.2.2 is to connect to 127.0.0.1 using android emulator
             socket = new Socket("10.0.2.2", 8888);
+            
+            // If it connects, this will display
             System.out.println("Connected");
         } catch (IOException e) {
 
