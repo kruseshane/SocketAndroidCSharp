c2451173d9045be34564095521d8255953d393a6
kruseshane <32747753+kruseshane@users.noreply.github.com>
2019-03-27 20:15:22 -0400

    Add comments to Program.cs

--- Socket/Program.cs
+++ Socket/Program.cs
@@ -10,11 +10,18 @@ namespace Socket
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            // Object which will serve as the socket using port 8888 (random port)
             TcpListener serverSocket = new TcpListener(8888);
+            
+            // Start socket
             serverSocket.Start();
             Console.WriteLine("Server Started... Awaiting connection");
+            
+            // Program hangs here until a connection is made
             serverSocket.AcceptSocket();
+            
+            // If a connection is made, this will display
             Console.WriteLine("Device Connected");
             Console.ReadKey(true);
         }
