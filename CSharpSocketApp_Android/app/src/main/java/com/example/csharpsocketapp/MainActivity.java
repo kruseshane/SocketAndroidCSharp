55e90b4dd30f904e3ad987ceef73b896c69ca58c
kruseshane <32747753+kruseshane@users.noreply.github.com>
2019-03-27 20:16:21 -0400

    Add comments to MainActivity.java

--- CSharpSocketApp_Android/app/src/main/java/com/example/csharpsocketapp/MainActivity.java
+++ CSharpSocketApp_Android/app/src/main/java/com/example/csharpsocketapp/MainActivity.java
@@ -10,7 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create object which extends AsyncTask
         ConnectionClass connection = new ConnectionClass();
+        
+        // Start the AsyncTask object
         connection.execute();
     }
 }
